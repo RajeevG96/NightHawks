6a1a428b14363dcb6abc090e78461296d3021b92
Itsfranhere <franciscofigueira@claromentis.com>
2017-02-24 14:08:09 +0000

    Smooth scrolling added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
-var elem = document.getElementById("backgroundvid");
-if (elem.requestFullscreen) {
-  elem.requestFullscreen();
-} else if (elem.mozRequestFullScreen) {
-  elem.mozRequestFullScreen();
-} else if (elem.webkitRequestFullscreen) {
-  elem.webkitRequestFullscreen();
-}
+$(document).ready(function () {
+
+var elem   = $('#bs-example-navbar-collapse-1');  
+    
+var bottom = $(elem).position().top+$(elem).outerHeight(true)
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > bottom) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+
+    $('#back-to-top').click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 500);
+        return false;
+    });
+
+});
