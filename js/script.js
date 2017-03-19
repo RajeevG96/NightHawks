201bf41a6d1c508510af98dc4897c9b1068f32fb
Itsfranhere <franciscofigueira@claromentis.com>
2017-03-19 18:47:00 +0000

    Copyright amended at bottom of the page + script to define year automatically

--- js/script.js
+++ js/script.js
@@ -18,3 +18,11 @@ var bottom = $(elem).position().top+$(elem).outerHeight(true)
     });
 
 });
+
+$(document).ready(function () {
+    // returns the year (four digits)
+    var currentyear = currentTime.getFullYear()
+
+    // write output MM/dd/yyyy
+    document.getElementById("year").innerHTML = currentyear;
+})
